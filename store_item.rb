b0c180ca7b65e6878ace789bb9df6e47b67f998c
Mike <mikehammond32@gmail.com>
2019-06-18 20:14:04 -0400

    Did some class work here

---
+++ store_item.rb
@@ -0,0 +1,61 @@
+# Taffy1 = {
+#   flavor: "watermelon" ,
+#   price: 2,
+#   brand: "Laffy Taffy" ,
+
+# }
+# Taffy2 = {
+#   flavor: "cherry" ,
+#   price: 2 ,
+#   brand: "Mamas Saltwater"
+# }
+# Taffy3 = {
+#   flavor: "lime" ,
+#   price: 3 ,
+#   brand: "Mamas Saltwater"
+# }
+
+# p Taffy1[:flavor]
+# p Taffy2[:price]
+# p Taffy3[:brand]
+
+class Taffy
+  def initialize(flavor, price, brand)
+  @flavor = flavor
+  @brand = brand
+  @price = price
+  end
+
+
+  def flavor
+    @flavor
+  end
+
+  def price
+    @price
+  end
+  
+  def set_price=(price)
+    @price = input_price
+  end
+    
+
+  def brand
+  @brand
+  end
+
+
+
+end
+
+Taffy1 = Taffy.new("watermelon", 2, "Laffy")
+
+p Taffy1
+
+Taffy2= Taffy.new("cherry", 2, "Mamas Saltwater")
+
+Taffy3= Taffy.new("lime", 3, "Mamas Saltwater")
+
+p Taffy2.flavor
+
+
